eeef22d698062ab9a63ff3c12c2f0c7b862b8b26
miscoder001 <38873656+miscoder001@users.noreply.github.com>
2023-12-28 16:55:19 +0800

    訂單查詢範例

--- src/main/java/tw/mymis/mywebdb/controller/OrderController.java
+++ src/main/java/tw/mymis/mywebdb/controller/OrderController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import tw.mymis.mywebdb.service.OrderService;
 
 @Controller
@@ -30,13 +31,21 @@ public class OrderController
         return "orders_list";
 
     }
-
-    @GetMapping("/qc")
-    public String toQuery() {
-
-        return "Query_Customer";
+    @GetMapping("/order_by_no")
+    public String getByCustomer(Model model, @RequestParam int ono) {
+        model.addAttribute("orders", orderService.getOrderById(ono));
+        return "orders_list";
 
     }
 
+
+    @GetMapping("/qc")
+    public String toQuery() {
+        return "Query_Customer";
+    }
+    @GetMapping("/qono")
+    public String QueryByOrderno() {
+        return "Query_Orderno";
+    }
 
 }
--- src/main/java/tw/mymis/mywebdb/service/CustomerService.java
+++ src/main/java/tw/mymis/mywebdb/service/CustomerService.java
@@ -3,7 +3,6 @@ package tw.mymis.mywebdb.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import tw.mymis.mywebdb.mapper.CustomerMapper;
 import tw.mymis.mywebdb.model.Customer;
 
